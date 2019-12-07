fe4a458e7c5a391d26707f2ac99e06d1622588ba
Amine-dd <52875495+Amine-dd@users.noreply.github.com>
2019-12-07 15:31:06 +0100

    Update Training2.py

--- Training2.py
+++ Training2.py
@@ -5,8 +5,7 @@ Created on Mon Nov 25 00:03:29 2019
 @author: pc
 """
 
-import sys
-sys.path.append('C:\\Users\\pc\\Desktop\\Machine Learning\\Learning\\ArtificialNeuralNetworks')
+
 from Fresh import NeuralNetMLP
 
 nn = NeuralNetMLP(n_output=10,n_features = X_train.shape[1],n_hidden=50,l2=0.1,l1=0.0,epochs=1000,eta=0.001,alpha=0.001,decrease_const=0.00001,shuffle=True,minibatches=50,random_state=1)
